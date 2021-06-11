cf5b5a9c1294806495def2c45202bf206b1f81ed
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-06-11 01:18:53 -0500

    Se modifico la vista index en travelloApp.views

--- travelloApp/views.py
+++ travelloApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from travelloApp.models import Destination
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    destinos = Destination.objects.all()
+    return render(request, "index.html", {"destinos": destinos})
